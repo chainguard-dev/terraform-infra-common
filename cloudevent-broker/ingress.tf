d9ade32290e55ec2b98d1e25fda1fd83f44fee10
Jason Hall <jason@chainguard.dev>
2023-12-19 14:33:56 -0500

    drop unused project_id
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- cloudevent-broker/ingress.tf
+++ cloudevent-broker/ingress.tf
@@ -83,6 +83,5 @@ resource "google_cloud_run_v2_service" "this" {
 
 module "ingress-dashboard" {
   source       = "../dashboard/service"
-  project_id   = var.project_id
   service_name = google_cloud_run_v2_service.this.name
 }
