71f8e0bb425dac3a29bf578fe30c3bf3dc36fbee
Jason Hall <jason@chainguard.dev>
2023-12-19 14:41:36 -0500

    drop unused param
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- cloudevent-recorder/recorder.tf
+++ cloudevent-recorder/recorder.tf
@@ -123,6 +123,5 @@ module "triggers" {
 
 module "recorder-dashboard" {
   source       = "../dashboard/service"
-  project_id   = var.project_id
   service_name = google_cloud_run_v2_service.recorder-service.name
 }
