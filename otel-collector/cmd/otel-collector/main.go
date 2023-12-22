1fddd0a29c74f298e1073f0186abed318c7d69cc
Nghia Tran <tcnghia@gmail.com>
2023-12-22 14:04:01 -0800

    Add /quitquitquit handler to otel-col (#35)

--- otel-collector/cmd/otel-collector/main.go
+++ otel-collector/cmd/otel-collector/main.go
@@ -6,7 +6,9 @@ SPDX-License-Identifier: Apache-2.0
 package main
 
 import (
+	"context"
 	"log"
+	"net/http"
 	"os"
 	"os/exec"
 )
@@ -18,11 +20,30 @@ func main() {
 		log.Fatalf("error checking config.yaml: %v", err)
 	}
 
+	log.Printf("config.yaml found at %s", configPath)
+
+	ctx, cancel := context.WithCancel(context.Background())
+
 	// run /otel-collector binary with --config config.yaml
-	cmd := exec.Command("otelcol-contrib", "--config", configPath)
+	cmd := exec.CommandContext(ctx, "otelcol-contrib", "--config", configPath)
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	if err := cmd.Run(); err != nil {
+
+	// Start the command but don't wait.
+	if err := cmd.Start(); err != nil {
+		log.Fatalf("failed to start otel-collector: %v", err)
+	}
+
+	// Start a server that serves /quitquitquit on port 31415
+	mux := http.NewServeMux()
+	srv := &http.Server{Addr: ":31415", Handler: mux}
+	mux.HandleFunc("/quitquitquit", func(w http.ResponseWriter, r *http.Request) {
+		go srv.Shutdown(ctx)
+		cancel()
+	})
+	go srv.ListenAndServe()
+
+	if err := cmd.Wait(); err != nil {
 		log.Fatal(err)
 	}
 }
