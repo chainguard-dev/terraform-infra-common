5cc8e2d397567184732d21247315a140e2ca884c
Lewis Denham-Parry <lewis@chainguard.dev>
2023-12-22 14:48:51 +0000

    Export 1d592083ff96db6815e0367842a21a866ab0e376
    Export:  1d592083ff96db6815e0367842a21a866ab0e376

--- dashboard/service/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "service_name" {
-  description = "Name of the service(s) to monitor"
-  type        = string
-}
-
-variable "labels" {
-  description = "Additional labels to apply to the dashboard."
-  default     = {}
-}
