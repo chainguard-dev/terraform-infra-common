9ea6c7543c2bafb9c7d044a3fd0770d1fad449e9
Jason Hall <jason@chainguard.dev>
2023-12-26 10:16:29 -0500

    Export f3bd4cb5030b429ff64fd2bbcf1d40dac5d3be07
    Export:  f3bd4cb5030b429ff64fd2bbcf1d40dac5d3be07

---
+++ dashboard/widgets/latency/main.tf
@@ -0,0 +1,39 @@
+variable "title" { type = string }
+variable "group_by_fields" { default = [] }
+variable "filter" { type = list(string) }
+
+locals {
+  bands = [50, 95, 99]
+}
+
+// https://cloud.google.com/monitoring/api/ref_v3/rest/v1/projects.dashboards#XyChart
+output "widget" {
+  value = {
+    title = var.title
+    xyChart = {
+      chartOptions = { mode = "COLOR" }
+      dataSets = [for band in local.bands : {
+        minAlignmentPeriod = "60s"
+        plotType           = "LINE"
+        targetAxis         = "Y1"
+        legendTemplate     = "${band}th %ile"
+        timeSeriesQuery = {
+          timeSeriesFilter = {
+            aggregation = {
+              alignmentPeriod    = "60s"
+              perSeriesAligner   = "ALIGN_DELTA"
+              crossSeriesReducer = "REDUCE_PERCENTILE_${band}"
+              groupByFields      = var.group_by_fields
+            }
+            filter = join("\n", var.filter)
+          }
+        }
+      }]
+      timeshiftDuration = "0s"
+      yAxis = {
+        label = "y1Axis"
+        scale = "LINEAR"
+      }
+    }
+  }
+}
