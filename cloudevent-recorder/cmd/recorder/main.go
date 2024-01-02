8a3165aab49b37c837a8d90a60f50c489cdd40d3
Billy Lynch <1844673+wlynch@users.noreply.github.com>
2024-01-02 15:51:55 -0500

    Export b90c83161f4e95d5fece5c7e92d49ac7bbc0c7b7
    Export:  b90c83161f4e95d5fece5c7e92d49ac7bbc0c7b7

---
+++ cloudevent-recorder/cmd/recorder/main.go
@@ -0,0 +1,47 @@
+/*
+Copyright 2023 Chainguard, Inc.
+SPDX-License-Identifier: Apache-2.0
+*/
+
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"os/signal"
+	"path/filepath"
+
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/kelseyhightower/envconfig"
+)
+
+type envConfig struct {
+	Port    int    `envconfig:"PORT" default:"8080" required:"true"`
+	LogPath string `envconfig:"LOG_PATH" required:"true"`
+}
+
+func main() {
+	var env envConfig
+	if err := envconfig.Process("", &env); err != nil {
+		log.Panicf("failed to process env var: %s", err)
+	}
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer cancel()
+
+	c, err := cloudevents.NewClientHTTP(cloudevents.WithPort(env.Port))
+	if err != nil {
+		log.Panicf("failed to create event client, %v", err)
+	}
+	if err := c.StartReceiver(ctx, func(ctx context.Context, event cloudevents.Event) error {
+		dir := filepath.Join(env.LogPath, event.Type())
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			return err
+		}
+
+		filename := filepath.Join(dir, event.ID())
+		return os.WriteFile(filename, event.Data(), 0600)
+	}); err != nil {
+		log.Panicf("failed to start event receiver, %v", err)
+	}
+}
