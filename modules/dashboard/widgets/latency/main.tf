9f3025e3a264650383bdaa4e0249e82990e57ef0
Kenny Leung <kleung@chainguard.dev>
2024-01-31 13:44:50 -0800

    use only single band for latency graph
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- modules/dashboard/widgets/latency/main.tf
+++ modules/dashboard/widgets/latency/main.tf
@@ -1,10 +1,7 @@
 variable "title" { type = string }
 variable "group_by_fields" { default = [] }
 variable "filter" { type = list(string) }
-
-locals {
-  bands = [50, 95, 99]
-}
+variable "band" { type = number, default = 99 }
 
 // https://cloud.google.com/monitoring/api/ref_v3/rest/v1/projects.dashboards#XyChart
 output "widget" {
@@ -12,17 +9,17 @@ output "widget" {
     title = var.title
     xyChart = {
       chartOptions = { mode = "COLOR" }
-      dataSets = [for band in local.bands : {
+      dataSets = [{
         minAlignmentPeriod = "60s"
         plotType           = "LINE"
         targetAxis         = "Y1"
-        legendTemplate     = "${band}th %ile"
+        legendTemplate     = "${var.band}th %ile"
         timeSeriesQuery = {
           timeSeriesFilter = {
             aggregation = {
               alignmentPeriod    = "60s"
               perSeriesAligner   = "ALIGN_DELTA"
-              crossSeriesReducer = "REDUCE_PERCENTILE_${band}"
+              crossSeriesReducer = "REDUCE_PERCENTILE_${var.band}"
               groupByFields      = var.group_by_fields
             }
             filter = join("\n", var.filter)
