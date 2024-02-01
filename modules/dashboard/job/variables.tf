6676e0a4e2560d00fe2f25090b34e7f8adf78cbb
stgarf <stgarf@users.noreply.github.com>
2024-02-01 00:15:04 -0800

    Export bc51cc12ccc1e65619a71b9f0acb77f51ab45990
    Export:  bc51cc12ccc1e65619a71b9f0acb77f51ab45990

--- modules/dashboard/job/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "job_name" {
-  description = "Name of the job(s) to monitor"
-  type        = string
-}
-
-variable "labels" {
-  description = "Additional labels to apply to the dashboard."
-  default     = {}
-}
