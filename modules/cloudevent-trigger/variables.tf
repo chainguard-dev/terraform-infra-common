f48819b229e7cdf9abe775d58c278b6e210428c9
Nghia Tran <tcnghia@gmail.com>
2024-02-04 14:23:30 -0800

    Export 1773f8a553c7aaf16c5e3ac9c57dbd89a2c55a80
    Export:  1773f8a553c7aaf16c5e3ac9c57dbd89a2c55a80

---
+++ modules/cloudevent-trigger/variables.tf
@@ -0,0 +1,37 @@
+variable "project_id" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "broker" {
+  description = "The name of the pubsub topic we are using as a broker."
+  type        = string
+}
+
+variable "filter" {
+  description = "A Knative Trigger-style filter over the cloud event attributes."
+  type        = map(string)
+}
+
+variable "private-service" {
+  description = "The private cloud run service that is subscribing to these events."
+  type = object({
+    name   = string
+    region = string
+  })
+}
+
+variable "dead_letter_policy" {
+  description = "The dead letter policy for the subscription."
+  type = object({
+    dead_letter_topic     = optional(string, null)
+    max_delivery_attempts = optional(number, 3)
+  })
+  default = {
+    dead_letter_topic     = null
+    max_delivery_attempts = 3
+  }
+}
