db99b43d14f1b7713b39c5fec240d1a622eb6ae5
Kenny Leung <k4leung4@gmail.com>
2024-03-11 16:57:18 -0700

    Export 0c3f6746e013a876010f3219cf4922d03c801a3b
    Export:  0c3f6746e013a876010f3219cf4922d03c801a3b

---
+++ modules/audit-serviceaccount/variables.tf
@@ -0,0 +1,25 @@
+variable "project_id" {
+  type = string
+}
+
+variable "service-account" {
+  description = "The email of the service account being audited."
+  type        = string
+}
+
+variable "allowed_principals" {
+  description = "The list of principals authorized to assume this identity."
+  type        = list(string)
+  default     = []
+}
+
+variable "allowed_principal_regex" {
+  description = "A regular expression to match allowed principals."
+  type        = string
+  default     = ""
+}
+
+variable "notification_channels" {
+  description = "The list of notification channels to alert when this policy fires."
+  type        = list(string)
+}
