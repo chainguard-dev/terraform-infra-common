e8d46f9fef2e1c99b5be72db747082185589424f
Jason Hall <jason@chainguard.dev>
2024-03-19 16:59:52 -0400

    fix schemas
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- modules/github-events/schemas/event_types.go
+++ modules/github-events/schemas/event_types.go
@@ -21,11 +21,26 @@ type Repo struct {
 	Name         string
 }
 
+type PullRequest struct {
+	State          string
+	Title          string
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	Draft          bool
+	Merged         bool
+	MergeCommitSHA string `json:"merge_commit_sha"`
+}
+
+// https://docs.github.com/en/actions/using-workflows/events-that-trigger-workflows#pull_request
+// https://pkg.go.dev/github.com/google/go-github/v60/github#PullRequestEvent
 type PullRequestEvent struct {
-	Action string // assigned,opened  etc.
-	User   User
-	Number int
-	Repo   Repo
+	Action   string // assigned,opened  etc.
+	Sender   User
+	Assignee User
+	Number   int
+	Repo     Repo
+
+	PullRequest PullRequest `json:"pull_request" bigquery:"pullRequest"`
 
 	// Populated when action is synchronize
 	Before string
@@ -36,16 +51,16 @@ type Workflow struct {
 	ID        int64 `bigquery:"id"`
 	Name      string
 	Path      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 	State     string
 }
 
 type WorkflowRun struct {
-	ID         int64 `bigquery:"id"`
-	RunNumber  int64
-	RunAttempt int64
-	HeadBranch string
+	ID         int64  `bigquery:"id"`
+	RunNumber  int64  `bigquery:"runNumber"`
+	RunAttempt int64  `bigquery:"runAttempt"`
+	HeadBranch string `bigquery:"headBranch"`
 	HeadSHA    string `bigquery:"headSHA"`
 
 	Name       string
@@ -54,10 +69,12 @@ type WorkflowRun struct {
 }
 
 // https://docs.github.com/developers/webhooks-and-events/webhook-events-and-payloads#workflow_run
+// subset of https://pkg.go.dev/github.com/google/go-github/v60/github#WorkflowRunEvent
 type WorkflowRunEvent struct {
-	Action       string // completed, etc.
-	Workflow     Workflow
-	Organization Organization
-	Repo         Repo
-	User         User
+	Action      string // completed, etc.
+	Workflow    Workflow
+	WorkflowRun WorkflowRun
+	Org         Organization
+	Repo        Repo
+	Sender      User
 }
