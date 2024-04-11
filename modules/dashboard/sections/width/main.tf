afcabfe82d467b9e79756f33f946255d25537c3b
Jason Hall <jason@chainguard.dev>
2024-04-11 17:15:52 -0400

    Export 5cb112a1fd10f972e791f8e79c2ad1f95599b810
    Export:  5cb112a1fd10f972e791f8e79c2ad1f95599b810

---
+++ modules/dashboard/sections/width/main.tf
@@ -0,0 +1,3 @@
+output "size" {
+  value = 12
+}
---
+++ modules/github-bots/time/main.go
@@ -0,0 +1,5 @@
+package main
+
+import "github.com/chainguard-dev/terraform-infra-common/modules/github-bots/sdk"
+
+func main() { sdk.Serve(New()) }
