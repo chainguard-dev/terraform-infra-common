e310b6c5ab41ded68f8a0a7c5e206a6b97c0f84d
Jason Hall <jason@chainguard.dev>
2024-06-08 10:11:51 -0400

    Export 786e7ae50f43952bfcd757af079f32ba629d6d14
    Export:  786e7ae50f43952bfcd757af079f32ba629d6d14

---
+++ modules/cloudevent-broker/variables.tf
@@ -0,0 +1,35 @@
+variable "project_id" {
+  type = string
+}
+
+variable "name" {
+  type = string
+}
+
+variable "regions" {
+  description = "A map from region names to a network and subnetwork.  A pub/sub topic and ingress service (publishing to the respective topic) will be created in each region, with the ingress service configured to egress all traffic via the specified subnetwork."
+  type = map(object({
+    network = string
+    subnet  = string
+  }))
+}
+
+variable "notification_channels" {
+  description = "List of notification channels to alert."
+  type        = list(string)
+}
+
+variable "limits" {
+  description = "Resource limits for the regional go service."
+  type = object({
+    cpu    = string
+    memory = string
+  })
+  default = null
+}
+
+variable "enable_profiler" {
+  type        = bool
+  default     = false
+  description = "Enable cloud profiler."
+}
