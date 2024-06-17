662e92abd667270beb790a721f360c64b1266993
Kenny Leung <k4leung4@gmail.com>
2024-06-17 13:52:04 -0700

    Export 12bd6f992b9676096cfb451291f1540f93fe2671
    Export:  12bd6f992b9676096cfb451291f1540f93fe2671

---
+++ modules/cloudevent-trigger/monitoring.tf
@@ -0,0 +1,33 @@
+resource "google_monitoring_alert_policy" "pubsub_dead_letter_queue_messages" {
+  alert_strategy {
+    auto_close = "3600s" // 1 hour
+  }
+
+  combiner = "OR"
+
+  conditions {
+    condition_threshold {
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_MAX"
+      }
+
+      comparison = "COMPARISON_GT"
+      duration   = "0s"
+      filter     = "metric.type=\"pubsub.googleapis.com/topic/send_request_count\" resource.type=\"pubsub_topic\" metadata.system_labels.\"name\"=\"${google_pubsub_topic.dead-letter.name}\""
+
+      trigger {
+        count = "1"
+      }
+
+      // TODO: make configurable later
+      threshold_value = 1
+    }
+
+    display_name = "${var.name}-${var.private-service.region}: dead-letter queue messages above 1"
+  }
+  display_name = "${var.name}-${var.private-service.region}: dead-letter queue messages above 1"
+
+  enabled               = var.dead_letter_queue_alert_enabled
+  notification_channels = var.notification_channels
+}
