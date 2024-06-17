aed34eff235fa750c894efa70c9b4fa1ba325c2d
Kenny Leung <kleung@chainguard.dev>
2024-06-17 16:26:51 -0700

    alert for cloud run server failure rate
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- modules/alerting/variables.tf
+++ modules/alerting/variables.tf
@@ -11,3 +11,9 @@ variable "oom_filter" {
   description = "additional filter to apply to oom alert policy"
   type        = string
 }
+
+variable "failure_rate_ratio_threshold" {
+  description = "ratio threshold to alert for cloud run server failure rate."
+  type        = number
+  default     = 0.2
+}
