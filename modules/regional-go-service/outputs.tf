b3a34989f8d4a671bb5db4b8331fb351fbee72b0
Kenny Leung <kleung@chainguard.dev>
2024-07-01 09:31:14 -0700

    output more cloudrun service properties:
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- modules/regional-go-service/outputs.tf
+++ modules/regional-go-service/outputs.tf
@@ -1,3 +1,11 @@
 output "names" {
   value = module.this.names
 }
+
+output "locations" {
+  value = module.this.locations
+}
+
+output "uris" {
+  value = module.this.uris
+}
--- modules/regional-service/outputs.tf
+++ modules/regional-service/outputs.tf
@@ -3,3 +3,15 @@ output "names" {
     for k, v in google_cloud_run_v2_service.this : k => v.name
   }
 }
+
+output "locations" {
+  value = {
+    for k, v in google_cloud_run_v2_service.this : k => v.location
+  }
+}
+
+output "uris" {
+  value = {
+    for k, v in google_cloud_run_v2_service.this : k => v.uri
+  }
+}
