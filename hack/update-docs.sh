b5a1c0d3762d7ac9b12ac8f9e21aac92618c99e1
Carlos Tadeu Panato Junior <ctadeu@gmail.com>
2024-07-11 16:32:51 +0200

    Export 47ea3b9c6f721f68f40b9deeb56bda8c900a55c8
    Export:  47ea3b9c6f721f68f40b9deeb56bda8c900a55c8

--- hack/update-docs.sh
+++ hack/update-docs.sh
@@ -1,13 +1,26 @@
-#!/bin/bash
-set -e
+#!/usr/bin/env bash
+
+set -o errexit
+
+# Find all directories containing .tf files
+directories=$(find . -name '*.tf' -exec dirname {} \;)
+
+# Check if the find command found any directories
+if [[ -z "${directories}" ]]; then
+  echo "No .tf files found."
+  exit 0
+fi
+
+sorted=$(echo "${directories}" | sort)
+sorted=$(echo "${sorted}" | uniq)
 
 # Update terraform docs
-for d in `find . -name '*.tf' -exec dirname {} \; | sort | uniq`; do
+for d in ${sorted}; do
     echo "###############################################"
-    echo "# Generating $d"
+    echo "# Generating ${d}"
     terraform-docs markdown table \
         --output-file README.md \
         --output-mode inject \
         --lockfile=false \
-        $d
+        "${d}"
 done
