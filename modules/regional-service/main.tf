863ddd40925756c7c8f52c9fc558422253d87de1
Kenny Leung <k4leung4@gmail.com>
2024-08-14 15:33:58 -0700

    startup probe (#498)
    to allow flexibility on how long startup can take
    
    to address
    ```
    │ Error: Error waiting for Updating Service: Error code 13, message: Revision 'enforce-staging-ing-vuln-01827-gfw' is not ready and cannot serve traffic. The user-provided container failed to start and listen on the port defined provided by the PORT=8080 environment variable. Logs for this revision might contain more information.
    │ 
    │ Logs URL: https://console.cloud.google.com/logs/viewer?project=staging-enforce-cd1e&resource=cloud_run_revision/service_name/enforce-staging-ing-vuln/revision_name/enforce-staging-ing-vuln-01827-gfw&advancedFilter=resource.type%3D%22cloud_run_revision%22%0Aresource.labels.service_name%3D%22enforce-staging-ing-vuln%22%0Aresource.labels.revision_name%3D%22enforce-staging-ing-vuln-01827-gfw%22 
    │ For more troubleshooting guidance, see https://cloud.google.com/run/docs/troubleshooting#container-failed-to-start
    ```
    
    where it may take a while to load the database on startup
    
    ---------
    
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- modules/regional-service/main.tf
+++ modules/regional-service/main.tf
@@ -136,6 +136,15 @@ resource "google_cloud_run_v2_service" "this" {
         }
       }
 
+      dynamic "startup_probe" {
+        for_each = local.main_container.startup_probe != null ? { "" : local.main_container.startup_probe } : {}
+        content {
+          initial_delay_seconds = startup_probe.value.initial_delay_seconds
+          timeout_seconds       = startup_probe.value.timeout_seconds
+          period_seconds        = startup_probe.value.period_seconds
+          failure_threshold     = startup_probe.value.failure_threshold
+        }
+      }
     }
 
     // Now the sidecar containers can be added.
@@ -186,6 +195,16 @@ resource "google_cloud_run_v2_service" "this" {
           content {
             name       = volume_mounts.value.name
             mount_path = volume_mounts.value.mount_path
+          }
+        }
+
+        dynamic "startup_probe" {
+          for_each = containers.value.startup_probe != null ? { "" : containers.value.startup_probe } : {}
+          content {
+            initial_delay_seconds = startup_probe.value.initial_delay_seconds
+            timeout_seconds       = startup_probe.value.timeout_seconds
+            period_seconds        = startup_probe.value.period_seconds
+            failure_threshold     = startup_probe.value.failure_threshold
           }
         }
       }
