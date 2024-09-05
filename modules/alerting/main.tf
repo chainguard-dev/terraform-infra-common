0262440295fb190b881d3d46560084186e205bff
Kenny Leung <kleung@chainguard.dev>
2024-09-05 14:34:39 -0700

    comment
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- modules/alerting/main.tf
+++ modules/alerting/main.tf
@@ -363,12 +363,12 @@ resource "google_logging_metric" "cloud-run-scaling-failure" {
 }
 
 resource "google_monitoring_alert_policy" "cloud-run-scaling-failure" {
-  # In the absence of data, incident will auto-close after an daily
+  # In the absence of data, incident will auto-close after an hour
   alert_strategy {
     auto_close = "3600s"
 
     notification_rate_limit {
-      period = "3600s" // re-alert daily if condition still valid.
+      period = "3600s" // re-alert hourly if condition still valid.
     }
   }
 
@@ -430,12 +430,12 @@ resource "google_logging_metric" "cloud-run-failed-req" {
 }
 
 resource "google_monitoring_alert_policy" "cloud-run-failed-req" {
-  # In the absence of data, incident will auto-close after an daily
+  # In the absence of data, incident will auto-close after an hour
   alert_strategy {
     auto_close = "3600s"
 
     notification_rate_limit {
-      period = "3600s" // re-alert daily if condition still valid.
+      period = "3600s" // re-alert hourly if condition still valid.
     }
   }
 
