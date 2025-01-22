68d6a3b35ebb3b4fa04a7bd4543613d1a40fa241
Kenny Leung <kleung@chainguard.dev>
2025-01-22 11:59:39 -0800

    turn off private nodes for cluster
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- modules/gke/main.tf
+++ modules/gke/main.tf
@@ -134,7 +134,7 @@ resource "google_container_cluster" "this" {
   }
 
   private_cluster_config {
-    enable_private_nodes    = true
+    enable_private_nodes    = false
     enable_private_endpoint = false
     master_ipv4_cidr_block  = var.master_ipv4_cidr_block
     master_global_access_config {
