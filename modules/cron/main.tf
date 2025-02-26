chainguard-dev/terraform-infra-common

commit b171410759df6be9ab277b9c3eb5cdccfdb4d6eb

enable cron module to run non-Go images (#734)
Currently the cron module will perform a `ko` build atop a given base
image. This PR expands it to make `importpath` optional, in which case
we just run the base image itself.

This is useful in case we want to use `cron` module with `apko`, to
customize the command or the `run-as` user.

---------

Signed-off-by: Nghia Tran <tcnghia@gmail.com>

modules/cron/main.tf
@@ -3,6 +3,7 @@ terraform {
     ko          = { source = "ko-build/ko" }
     google      = { source = "hashicorp/google" }
     google-beta = { source = "hashicorp/google-beta" }
+    oci         = { source = "chainguard-dev/oci" }
   }
 }
 
@@ -26,11 +27,17 @@ locals {
 }
 
 resource "ko_build" "image" {
+  count       = var.importpath != "" ? 1 : 0
   importpath  = var.importpath
   working_dir = var.working_dir
   base_image  = var.base_image
   repo        = local.repo
   env         = var.ko_build_env
+}
+
+locals {
+  parsed = provider::oci::parse(var.base_image)
+  ref    = var.importpath == "" ? "${local.parsed.registry_repo}@${local.parsed.digest}" : ko_build.image[0].image_ref
 }
 
 resource "google_project_iam_member" "metrics-writer" {
@@ -109,7 +116,7 @@ resource "google_cloud_run_v2_job" "job" {
         }
       }
       containers {
-        image = ko_build.image.image_ref
+        image = local.ref
 
         resources {
           limits = {

modules/cron/variables.tf
@@ -35,7 +35,8 @@ variable "service_account" {
 
 variable "importpath" {
   type        = string
-  description = "The import path that contains the cron application."
+  description = "The import path that contains the cron application. Leave empty to run the unmodified base image as the application: for example, when running an `apko`-built image. This works by skipping the `ko` build and just use the base image directly in the cron job."
+  default     = ""
 }
 
 variable "ko_build_env" {

commit 8bf44159ec1ba1fdb91eb05777c952fd2866f221

cron: don't parse base_image when importpath="" (#735)
Because `oci::parse` only accepts digests, don't call it when
`importpath=""` for backward compatibility. I will follow up with
another fix to also accept tags for when `importpath=""`, but for now
this should fix the broken backward compatibility introduced in #734

Signed-off-by: Nghia Tran <tcnghia@gmail.com>

modules/cron/main.tf
@@ -36,7 +36,7 @@ resource "ko_build" "image" {
 }
 
 locals {
-  parsed = provider::oci::parse(var.base_image)
+  parsed = var.importpath == "" ? provider::oci::parse(var.base_image) : {}
   ref    = var.importpath == "" ? "${local.parsed.registry_repo}@${local.parsed.digest}" : ko_build.image[0].image_ref
 }
 

modules/cron/variables.tf
@@ -35,7 +35,7 @@ variable "service_account" {
 
 variable "importpath" {
   type        = string
-  description = "The import path that contains the cron application. Leave empty to run the unmodified base image as the application: for example, when running an `apko`-built image. This works by skipping the `ko` build and just use the base image directly in the cron job."
+  description = "The import path that contains the cron application. Leave empty to run the unmodified base image as the application: for example, when running an `apko`-built image. This works by skipping the `ko` build and just use the base image directly in the cron job. A digest must be specified in this case."
   default     = ""
 }
 