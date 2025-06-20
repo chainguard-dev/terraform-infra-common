2e0a22f87d7119abc2a8ae42b1755e5943c3406e
Kenny Leung <kleung@chainguard.dev>
2025-06-20 14:55:09 -0700

    fix
    Signed-off-by: Kenny Leung <kleung@chainguard.dev>

--- modules/dashboard/cloudevent-receiver/dashboard.tf
+++ modules/dashboard/cloudevent-receiver/dashboard.tf
@@ -153,13 +153,13 @@ module "trigger-dashboards" {
       {
         # for GCP Cloud Run built-in metrics
         filterType  = "RESOURCE_LABEL"
-        stringValue = var.name
+        stringValue = var.service_name
         labelKey    = "service_name"
       },
       {
         # for Prometheus user added metrics
         filterType  = "METRIC_LABEL"
-        stringValue = var.name
+        stringValue = var.service_name
         labelKey    = "service_name"
       },
     ]
