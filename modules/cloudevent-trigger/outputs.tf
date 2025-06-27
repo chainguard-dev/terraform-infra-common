bc244b3e3f092055eaca1e40a653b19df43438f2
Slavina S <3056639+blancadesal@users.noreply.github.com>
2025-06-27 09:34:27 +0200

    Export b3b098a553b907d1544b9fbc680b10a48badb645
    Export:  b3b098a553b907d1544b9fbc680b10a48badb645

--- modules/cloudevent-trigger/outputs.tf
+++
@@ -1,5 +0,0 @@
-output "dead-letter-broker" {
-  depends_on  = [google_pubsub_topic.dead-letter]
-  value       = google_pubsub_topic.dead-letter.name
-  description = "The name of the dead-letter topic, which is used to store events that could not be delivered."
-}
