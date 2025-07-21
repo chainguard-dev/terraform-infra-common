6b2e9aed904f4b5a298479be09a50d11112e52fd
Markus Thömmes <markus.thoemmes@chainguard.dev>
2025-07-21 10:30:15 +0200

    dashboard: Filter HTTP request count metric by revision (#963)
    Currently, we're not filtering the request count metric by revision,
    causing the dashboard to say "Unspecified resource" and the graph to be
    wildly wrong. This fixes this oversight.

--- modules/dashboard/sections/http/main.tf
+++ modules/dashboard/sections/http/main.tf
@@ -8,7 +8,7 @@ module "width" { source = "../width" }
 module "request_count" {
   source          = "../../widgets/xy"
   title           = "Request count"
-  filter          = concat(var.filter, ["metric.type=\"run.googleapis.com/request_count\""])
+  filter          = concat(var.filter, ["resource.type=\"cloud_run_revision\"", "metric.type=\"run.googleapis.com/request_count\""])
   group_by_fields = ["metric.label.\"response_code_class\""]
   primary_align   = "ALIGN_RATE"
   primary_reduce  = "REDUCE_SUM"
