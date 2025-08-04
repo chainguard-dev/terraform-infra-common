cfa7f75f9ae3f65cf324e4e992e96b4736d06cb1
Joe Borg <joe.borg@chainguard.dev>
2025-08-04 15:48:21 -0400

    Add required roles to make an SSH connection over IAP
    Signed-off-by: Joe Borg <joe.borg@chainguard.dev>

--- modules/bastion/main.tf
+++ modules/bastion/main.tf
@@ -275,6 +275,20 @@ resource "google_project_iam_member" "dev_os_login" {
   member   = each.key
 }
 
+resource "google_project_iam_member" "dev_iap_tunnel_resource_accessor" {
+  for_each = toset(var.dev_principals)
+  project  = var.project_id
+  role     = "roles/iap.tunnelResourceAccessor"
+  member   = each.key
+}
+
+resource "google_project_iam_member" "dev_service_account_user" {
+  for_each = toset(var.dev_principals)
+  project  = var.project_id
+  role     = "roles/iam.serviceAccountUser"
+  member   = each.key
+}
+
 resource "google_project_iam_member" "dev_cloudsql_client" {
   count   = var.install_sql_proxy ? length(distinct(var.dev_principals)) : 0
   project = var.project_id
