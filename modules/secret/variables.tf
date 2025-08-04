95bc79aa10f4648d15c1556778779376f5a48540
Joe Borg <joe.borg@chainguard.dev>
2025-08-04 15:57:40 -0400

    Export 302d81767f71c0554fce6d4d17ac53eab5dfa915
    Export:  302d81767f71c0554fce6d4d17ac53eab5dfa915

---
+++ modules/secret/variables.tf
@@ -0,0 +1,72 @@
+variable "project_id" {
+  type = string
+}
+
+variable "name" {
+  description = "The name to give the secret."
+  type        = string
+}
+
+variable "authorized-adder" {
+  description = "A member-style representation of the identity authorized to add new secret values (e.g. group:oncall@my-corp.dev)."
+  type        = string
+}
+
+variable "service-account" {
+  description = "(Deprecated: Use service-accounts instead) The email of the service account that will access the secret."
+  type        = string
+  default     = ""
+}
+
+variable "service-accounts" {
+  description = "The emails of the service accounts that will access the secret."
+  type        = list(string)
+  default     = []
+
+  validation {
+    # To support the legacy service-account variable, ensure that either that var is
+    # non-empty, or service-accounts is non-empty.
+    condition     = var.service-account != "" || length(var.service-accounts) > 0
+    error_message = "Must provide at least one value in service-accounts"
+  }
+}
+
+variable "notification-channels" {
+  description = "The channels to notify if the configuration data is improperly accessed."
+  type        = list(string)
+}
+
+variable "create_placeholder_version" {
+  description = "Whether to create a placeholder secret version to avoid bad reference on first deploy."
+  type        = bool
+  default     = false
+}
+
+variable "labels" {
+  description = "Labels to apply to the secret."
+  type        = map(string)
+  default     = {}
+}
+
+variable "require_squad" {
+  description = "Whether to require squad variable to be specified"
+  type        = bool
+  default     = false
+}
+
+variable "squad" {
+  description = "Squad label to apply to the secret."
+  type        = string
+  default     = ""
+
+  validation {
+    condition     = !var.require_squad || var.squad != ""
+    error_message = "squad must be specified or disable check by setting require_squad = false"
+  }
+}
+
+variable "product" {
+  description = "Product label to apply to the service."
+  type        = string
+  default     = "unknown"
+}
