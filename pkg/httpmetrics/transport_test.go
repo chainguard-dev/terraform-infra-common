258f81d3a1cb6557fc7a785d97e8e3c4843277e7
Joe Borg <joe.borg@chainguard.dev>
2025-08-04 17:59:15 -0400

    Export 36fc25935855ee6cb37bebe0db0edc9045e49903
    Export:  36fc25935855ee6cb37bebe0db0edc9045e49903

---
+++ pkg/httpmetrics/transport_test.go
@@ -0,0 +1,81 @@
+package httpmetrics
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/testutil"
+)
+
+func TestTransport(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+		t.Log("got request")
+	}))
+	defer s.Close()
+
+	resp, err := (&http.Client{Transport: Transport}).Get(s.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("want OK, got %s", resp.Status)
+	}
+
+	// Sample a metric to make sure labels are being properly applied.
+	if got := testutil.ToFloat64(mReqCount.With(prometheus.Labels{
+		"method":        http.MethodGet,
+		"code":          "200",
+		"host":          "other",
+		"service_name":  "unknown",
+		"revision_name": "unknown",
+		"ce_type":       "",
+	})); got != 1 {
+		t.Errorf("want metric count = 1, got %f", got)
+	}
+}
+
+func TestTransport_SkipBucketize(t *testing.T) {
+	s := httptest.NewServer(http.HandlerFunc(func(http.ResponseWriter, *http.Request) {
+		t.Log("got request")
+	}))
+	defer s.Close()
+
+	resp, err := (&http.Client{Transport: WrapTransport(http.DefaultTransport, WithSkipBucketize(true))}).Get(s.URL)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatalf("want OK, got %s", resp.Status)
+	}
+
+	// Sample a metric to make sure labels are being properly applied.
+	if got := testutil.ToFloat64(mReqCount.With(prometheus.Labels{
+		"method":        http.MethodGet,
+		"code":          "200",
+		"host":          "unbucketized",
+		"service_name":  "unknown",
+		"revision_name": "unknown",
+		"ce_type":       "",
+	})); got != 1 {
+		t.Errorf("want metric count = 1, got %f", got)
+	}
+}
+
+func TestExtractInnerTransport(t *testing.T) {
+	t.Run("not wrapped", func(t *testing.T) {
+		tr := &http.Transport{}
+		if got := ExtractInnerTransport(tr); got != tr {
+			t.Errorf("want %v, got %v", tr, got)
+		}
+	})
+
+	t.Run("wrapped", func(t *testing.T) {
+		inner := &http.Transport{}
+		var tr = WrapTransport(inner)
+		if got := ExtractInnerTransport(tr); got != inner {
+			t.Errorf("want %v, got %v", inner, got)
+		}
+	})
+}
