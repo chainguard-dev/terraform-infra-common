0982d9585d8fa583cc5657831308ba98da5fdb0b
Nghia Tran <tcnghia@gmail.com>
2025-08-27 12:01:42 -0700

    Update pkg/workqueue/gcs/metrics.go
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>

--- pkg/workqueue/gcs/metrics.go
+++ pkg/workqueue/gcs/metrics.go
@@ -100,7 +100,7 @@ var (
 		prometheus.HistogramOpts{
 			Name:    "workqueue_retry_attempts",
 			Help:    "Distribution of retry attempts across all tasks.",
-			Buckets: []float64{1, 2, 5, 10, 20, 30, 40, 50, 100, 200, 500, 1000},
+			Buckets: []float64{1, 2, 5, 10, 20, 50, 100, 200, 500, 1000},
 		},
 		[]string{"service_name", "revision_name"},
 	)
