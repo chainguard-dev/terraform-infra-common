a3f4c6dbb33a6723e868b5f0b26c17e74fa82120
Amelia Crate <amelia.crate@chainguard.dev>
2025-09-18 14:06:21 +0000

    Export 2fbee5f81f1138f0cb71e2963c8f7217bec7a1f6
    Export:  2fbee5f81f1138f0cb71e2963c8f7217bec7a1f6

--- modules/cloudevent-recorder/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "dataset_id" {
-  value = google_bigquery_dataset.this.dataset_id
-}
-
-output "table_ids" {
-  value = {
-    for k, v in google_bigquery_table.types : k => v.table_id
-  }
-}
