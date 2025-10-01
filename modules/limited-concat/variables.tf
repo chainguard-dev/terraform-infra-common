ed6501d0e43ffab093d06bcd6f5087dfcb6639b6
Colin Douglas <16968564+cmdpdx@users.noreply.github.com>
2025-10-01 14:05:56 -0700

    add a module to concat two strings, subject to a given maximum length (#1130)
    There are a few resources that have constraints on the length of their
    names. We currently apply the same name to a bunch of subresources
    within modules, and if one violates a maximum length requirement the
    entire module fails. This module intends to allow us to only shorten
    names when necessary, and otherwise continue using the caller provided
    names as is.
    
    Signed-off-by: Colin Douglas <colin@chainguard.dev>

---
+++ modules/limited-concat/variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+  description = "First part of the result, will be shortened if length(prefix)+length(suffix) > limit."
+  type = string
+}
+
+variable "suffix" {
+  description = "Second part of the result, included in whole."
+  type = string
+}
+
+variable "limit" {
+  description = "Maximum length of the resulting concatenation."
+  type = number
+
+  validation {
+    condition = var.limit >= length(var.suffix)
+    error_message = "limit cannot be less than the length of suffix."
+  }
+}
