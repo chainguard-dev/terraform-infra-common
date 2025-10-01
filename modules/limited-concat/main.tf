b4308413f53b2b6f1525abf992c75eec76c052ba
Sam Dacanay <sam.dacanay@chainguard.dev>
2025-10-01 15:37:09 -0700

    Export 62abcdf3029afd38c973044ee2f6c9d89ac1109e
    Export:  62abcdf3029afd38c973044ee2f6c9d89ac1109e

--- modules/limited-concat/main.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  # NB: substr returns the entire string if length is greater than the length of the string.
-  prefix = substr(var.prefix, 0, (var.limit - length(var.suffix)))
-}
-
-output "result" {
-  description = "The concatenation of prefix and suffix, with limit applied."
-  value = "${local.prefix}${var.suffix}"
-}
