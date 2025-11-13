bee3fea3d0643431fa31c6f7f8b5ef214659900f
Nghia Tran <tcnghia@gmail.com>
2025-11-13 12:02:14 -0800

    refactor: use regexp.MustCompile in global var instead of init
    Signed-off-by: Nghia Tran <tcnghia@gmail.com>

--- pkg/httpmetrics/github_api.go
+++ pkg/httpmetrics/github_api.go
@@ -5,7 +5,6 @@ package httpmetrics
 
 import (
 	"context"
-	"fmt"
 	"net/http"
 	"regexp"
 	"sync"
@@ -20,151 +19,133 @@ type pathPattern struct {
 	bucket  string
 }
 
-var githubAPIPatterns = []pathPattern{}
-
-func init() {
-	// Default GitHub API endpoint patterns.
-	// Based on GitHub REST API documentation: https://docs.github.com/en/rest
-	patterns := []struct {
-		regex  string
-		bucket string
-	}{{
-		// https://docs.github.com/en/rest/repos/repos#get-a-repository
-		regex:  `^/repos/[^/]+/[^/]+$`,
-		bucket: "/repos/{org}/{repo}",
-	}, {
-		// https://docs.github.com/en/rest/issues/issues#list-repository-issues
-		regex:  `^/repos/[^/]+/[^/]+/issues$`,
-		bucket: "/repos/{org}/{repo}/issues",
-	}, {
-		// https://docs.github.com/en/rest/issues/issues#get-an-issue
-		regex:  `^/repos/[^/]+/[^/]+/issues/\d+$`,
-		bucket: "/repos/{org}/{repo}/issues/{number}",
-	}, {
-		// https://docs.github.com/en/rest/issues/comments#list-issue-comments
-		regex:  `^/repos/[^/]+/[^/]+/issues/\d+/comments$`,
-		bucket: "/repos/{org}/{repo}/issues/{number}/comments",
-	}, {
-		// https://docs.github.com/en/rest/pulls/pulls#list-pull-requests
-		regex:  `^/repos/[^/]+/[^/]+/pulls$`,
-		bucket: "/repos/{org}/{repo}/pulls",
-	}, {
-		// https://docs.github.com/en/rest/pulls/pulls#get-a-pull-request
-		regex:  `^/repos/[^/]+/[^/]+/pulls/\d+$`,
-		bucket: "/repos/{org}/{repo}/pulls/{number}",
-	}, {
-		// https://docs.github.com/en/rest/pulls/pulls#list-pull-requests-files
-		regex:  `^/repos/[^/]+/[^/]+/pulls/\d+/files$`,
-		bucket: "/repos/{org}/{repo}/pulls/{number}/files",
-	}, {
-		// https://docs.github.com/en/rest/pulls/comments#list-review-comments-on-a-pull-request
-		regex:  `^/repos/[^/]+/[^/]+/pulls/\d+/comments$`,
-		bucket: "/repos/{org}/{repo}/pulls/{number}/comments",
-	}, {
-		// https://docs.github.com/en/rest/pulls/reviews#list-reviews-for-a-pull-request
-		regex:  `^/repos/[^/]+/[^/]+/pulls/\d+/reviews$`,
-		bucket: "/repos/{org}/{repo}/pulls/{number}/reviews",
-	}, {
-		// https://docs.github.com/en/rest/commits/commits#list-commits
-		regex:  `^/repos/[^/]+/[^/]+/commits$`,
-		bucket: "/repos/{org}/{repo}/commits",
-	}, {
-		// https://docs.github.com/en/rest/commits/commits#get-a-commit
-		regex:  `^/repos/[^/]+/[^/]+/commits/[^/]+$`,
-		bucket: "/repos/{org}/{repo}/commits/{sha}",
-	}, {
-		// https://docs.github.com/en/rest/commits/statuses#get-the-combined-status-for-a-specific-reference
-		regex:  `^/repos/[^/]+/[^/]+/commits/[^/]+/status$`,
-		bucket: "/repos/{org}/{repo}/commits/{sha}/status",
-	}, {
-		// https://docs.github.com/en/rest/commits/statuses#create-a-commit-status
-		regex:  `^/repos/[^/]+/[^/]+/statuses/[^/]+$`,
-		bucket: "/repos/{org}/{repo}/statuses/{sha}",
-	}, {
-		// https://docs.github.com/en/rest/repos/contents#get-repository-content
-		regex:  `^/repos/[^/]+/[^/]+/contents/.*$`,
-		bucket: "/repos/{org}/{repo}/contents/{path}",
-	}, {
-		// https://docs.github.com/en/rest/branches/branches#list-branches
-		regex:  `^/repos/[^/]+/[^/]+/branches$`,
-		bucket: "/repos/{org}/{repo}/branches",
-	}, {
-		// https://docs.github.com/en/rest/branches/branches#get-a-branch
-		regex:  `^/repos/[^/]+/[^/]+/branches/[^/]+$`,
-		bucket: "/repos/{org}/{repo}/branches/{branch}",
-	}, {
-		// https://docs.github.com/en/rest/repos/repos#list-repository-tags
-		regex:  `^/repos/[^/]+/[^/]+/tags$`,
-		bucket: "/repos/{org}/{repo}/tags",
-	}, {
-		// https://docs.github.com/en/rest/releases/releases#list-releases
-		regex:  `^/repos/[^/]+/[^/]+/releases$`,
-		bucket: "/repos/{org}/{repo}/releases",
-	}, {
-		// https://docs.github.com/en/rest/releases/releases#get-a-release
-		regex:  `^/repos/[^/]+/[^/]+/releases/\d+$`,
-		bucket: "/repos/{org}/{repo}/releases/{id}",
-	}, {
-		// https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-repository
-		regex:  `^/repos/[^/]+/[^/]+/actions/runs$`,
-		bucket: "/repos/{org}/{repo}/actions/runs",
-	}, {
-		// https://docs.github.com/en/rest/actions/workflow-runs#get-a-workflow-run
-		regex:  `^/repos/[^/]+/[^/]+/actions/runs/\d+$`,
-		bucket: "/repos/{org}/{repo}/actions/runs/{id}",
-	}, {
-		// https://docs.github.com/en/rest/actions/workflows#list-repository-workflows
-		regex:  `^/repos/[^/]+/[^/]+/actions/workflows$`,
-		bucket: "/repos/{org}/{repo}/actions/workflows",
-	}, {
-		// https://docs.github.com/en/rest/actions/workflows#get-a-workflow
-		regex:  `^/repos/[^/]+/[^/]+/actions/workflows/[^/]+$`,
-		bucket: "/repos/{org}/{repo}/actions/workflows/{id}",
-	}, {
-		// https://docs.github.com/en/rest/orgs/orgs#get-an-organization
-		regex:  `^/orgs/[^/]+$`,
-		bucket: "/orgs/{org}",
-	}, {
-		// https://docs.github.com/en/rest/repos/repos#list-organization-repositories
-		regex:  `^/orgs/[^/]+/repos$`,
-		bucket: "/orgs/{org}/repos",
-	}, {
-		// https://docs.github.com/en/rest/orgs/members#list-organization-members
-		regex:  `^/orgs/[^/]+/members$`,
-		bucket: "/orgs/{org}/members",
-	}, {
-		// https://docs.github.com/en/rest/teams/teams#list-teams
-		regex:  `^/orgs/[^/]+/teams$`,
-		bucket: "/orgs/{org}/teams",
-	}, {
-		// https://docs.github.com/en/rest/users/users#get-the-authenticated-user
-		regex:  `^/user$`,
-		bucket: "/user",
-	}, {
-		// https://docs.github.com/en/rest/repos/repos#list-repositories-for-the-authenticated-user
-		regex:  `^/user/repos$`,
-		bucket: "/user/repos",
-	}, {
-		// https://docs.github.com/en/rest/users/users#get-a-user
-		regex:  `^/users/[^/]+$`,
-		bucket: "/users/{user}",
-	}, {
-		// https://docs.github.com/en/rest/repos/repos#list-repositories-for-a-user
-		regex:  `^/users/[^/]+/repos$`,
-		bucket: "/users/{user}/repos",
-	}}
-
-	for _, p := range patterns {
-		re, err := regexp.Compile(p.regex)
-		if err != nil {
-			panic(fmt.Sprintf("failed to compile pattern %q: %v", p.regex, err))
-		}
-		githubAPIPatterns = append(githubAPIPatterns, pathPattern{
-			pattern: re,
-			bucket:  p.bucket,
-		})
-	}
-}
+// Default GitHub API endpoint patterns.
+// Based on GitHub REST API documentation: https://docs.github.com/en/rest
+var githubAPIPatterns = []pathPattern{{
+	// https://docs.github.com/en/rest/repos/repos#get-a-repository
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+$`),
+	bucket:  "/repos/{org}/{repo}",
+}, {
+	// https://docs.github.com/en/rest/issues/issues#list-repository-issues
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/issues$`),
+	bucket:  "/repos/{org}/{repo}/issues",
+}, {
+	// https://docs.github.com/en/rest/issues/issues#get-an-issue
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/issues/\d+$`),
+	bucket:  "/repos/{org}/{repo}/issues/{number}",
+}, {
+	// https://docs.github.com/en/rest/issues/comments#list-issue-comments
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/issues/\d+/comments$`),
+	bucket:  "/repos/{org}/{repo}/issues/{number}/comments",
+}, {
+	// https://docs.github.com/en/rest/pulls/pulls#list-pull-requests
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/pulls$`),
+	bucket:  "/repos/{org}/{repo}/pulls",
+}, {
+	// https://docs.github.com/en/rest/pulls/pulls#get-a-pull-request
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/pulls/\d+$`),
+	bucket:  "/repos/{org}/{repo}/pulls/{number}",
+}, {
+	// https://docs.github.com/en/rest/pulls/pulls#list-pull-requests-files
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/pulls/\d+/files$`),
+	bucket:  "/repos/{org}/{repo}/pulls/{number}/files",
+}, {
+	// https://docs.github.com/en/rest/pulls/comments#list-review-comments-on-a-pull-request
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/pulls/\d+/comments$`),
+	bucket:  "/repos/{org}/{repo}/pulls/{number}/comments",
+}, {
+	// https://docs.github.com/en/rest/pulls/reviews#list-reviews-for-a-pull-request
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/pulls/\d+/reviews$`),
+	bucket:  "/repos/{org}/{repo}/pulls/{number}/reviews",
+}, {
+	// https://docs.github.com/en/rest/commits/commits#list-commits
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/commits$`),
+	bucket:  "/repos/{org}/{repo}/commits",
+}, {
+	// https://docs.github.com/en/rest/commits/commits#get-a-commit
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/commits/[^/]+$`),
+	bucket:  "/repos/{org}/{repo}/commits/{sha}",
+}, {
+	// https://docs.github.com/en/rest/commits/statuses#get-the-combined-status-for-a-specific-reference
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/commits/[^/]+/status$`),
+	bucket:  "/repos/{org}/{repo}/commits/{sha}/status",
+}, {
+	// https://docs.github.com/en/rest/commits/statuses#create-a-commit-status
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/statuses/[^/]+$`),
+	bucket:  "/repos/{org}/{repo}/statuses/{sha}",
+}, {
+	// https://docs.github.com/en/rest/repos/contents#get-repository-content
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/contents/.*$`),
+	bucket:  "/repos/{org}/{repo}/contents/{path}",
+}, {
+	// https://docs.github.com/en/rest/branches/branches#list-branches
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/branches$`),
+	bucket:  "/repos/{org}/{repo}/branches",
+}, {
+	// https://docs.github.com/en/rest/branches/branches#get-a-branch
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/branches/[^/]+$`),
+	bucket:  "/repos/{org}/{repo}/branches/{branch}",
+}, {
+	// https://docs.github.com/en/rest/repos/repos#list-repository-tags
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/tags$`),
+	bucket:  "/repos/{org}/{repo}/tags",
+}, {
+	// https://docs.github.com/en/rest/releases/releases#list-releases
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/releases$`),
+	bucket:  "/repos/{org}/{repo}/releases",
+}, {
+	// https://docs.github.com/en/rest/releases/releases#get-a-release
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/releases/\d+$`),
+	bucket:  "/repos/{org}/{repo}/releases/{id}",
+}, {
+	// https://docs.github.com/en/rest/actions/workflow-runs#list-workflow-runs-for-a-repository
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/actions/runs$`),
+	bucket:  "/repos/{org}/{repo}/actions/runs",
+}, {
+	// https://docs.github.com/en/rest/actions/workflow-runs#get-a-workflow-run
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/actions/runs/\d+$`),
+	bucket:  "/repos/{org}/{repo}/actions/runs/{id}",
+}, {
+	// https://docs.github.com/en/rest/actions/workflows#list-repository-workflows
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/actions/workflows$`),
+	bucket:  "/repos/{org}/{repo}/actions/workflows",
+}, {
+	// https://docs.github.com/en/rest/actions/workflows#get-a-workflow
+	pattern: regexp.MustCompile(`^/repos/[^/]+/[^/]+/actions/workflows/[^/]+$`),
+	bucket:  "/repos/{org}/{repo}/actions/workflows/{id}",
+}, {
+	// https://docs.github.com/en/rest/orgs/orgs#get-an-organization
+	pattern: regexp.MustCompile(`^/orgs/[^/]+$`),
+	bucket:  "/orgs/{org}",
+}, {
+	// https://docs.github.com/en/rest/repos/repos#list-organization-repositories
+	pattern: regexp.MustCompile(`^/orgs/[^/]+/repos$`),
+	bucket:  "/orgs/{org}/repos",
+}, {
+	// https://docs.github.com/en/rest/orgs/members#list-organization-members
+	pattern: regexp.MustCompile(`^/orgs/[^/]+/members$`),
+	bucket:  "/orgs/{org}/members",
+}, {
+	// https://docs.github.com/en/rest/teams/teams#list-teams
+	pattern: regexp.MustCompile(`^/orgs/[^/]+/teams$`),
+	bucket:  "/orgs/{org}/teams",
+}, {
+	// https://docs.github.com/en/rest/users/users#get-the-authenticated-user
+	pattern: regexp.MustCompile(`^/user$`),
+	bucket:  "/user",
+}, {
+	// https://docs.github.com/en/rest/repos/repos#list-repositories-for-the-authenticated-user
+	pattern: regexp.MustCompile(`^/user/repos$`),
+	bucket:  "/user/repos",
+}, {
+	// https://docs.github.com/en/rest/users/users#get-a-user
+	pattern: regexp.MustCompile(`^/users/[^/]+$`),
+	bucket:  "/users/{user}",
+}, {
+	// https://docs.github.com/en/rest/repos/repos#list-repositories-for-a-user
+	pattern: regexp.MustCompile(`^/users/[^/]+/repos$`),
+	bucket:  "/users/{user}/repos",
+}}
 
 var seenPathMap = sync.Map{}
 
