7942d563e0061ecb5e582f994743df1ce266ecae
Billy Lynch <1844673+wlynch@users.noreply.github.com>
2026-08-03 17:18:01 -0400

    Export e7bd6ee0a802a799c8c27665017c2d0281b95726
    Export:  e7bd6ee0a802a799c8c27665017c2d0281b95726

--- modules/secret/main.tf
+++ modules/secret/main.tf
@@ -113,16 +113,18 @@ resource "google_monitoring_alert_policy" "anomalous-secret-access" {
       -- Ignore the identity that is intended to access this.
       -(
         protoPayload.authenticationInfo.principalEmail=~"${join("|", local.accessor_emails)}"
-        protoPayload.methodName=~"google.cloud.secretmanager.v1.SecretManagerService.(AccessSecretVersion|GetSecretVersion)"
+        protoPayload.methodName=~"google.cloud.secretmanager.v1.SecretManagerService.AccessSecretVersion"
       )
       -- Ignore the identities that are authorized to manipulate secret versions.
       -(
         protoPayload.authenticationInfo.principalEmail=~"${join("|", local.authorized_adder_emails)}"
         protoPayload.methodName=~"google.cloud.secretmanager.v1.SecretManagerService.(DestroySecretVersion|AddSecretVersion|EnableSecretVersion)"
       )
-      -- Ignore benign secret actions.
+      -- Ignore benign secret actions. ListSecretVersions and GetSecretVersion
+      -- only return version metadata (not the secret payload), so we don't care
+      -- who calls them.
       -(
-        protoPayload.methodName=~"google.cloud.secretmanager.v1.SecretManagerService.ListSecretVersions"
+        protoPayload.methodName=~"google.cloud.secretmanager.v1.SecretManagerService.(ListSecretVersions|GetSecretVersion)"
       )
       EOT
 
